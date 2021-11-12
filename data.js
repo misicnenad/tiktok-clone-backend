12c849bb70c11412e044fbf3663a3d9bb09dccc2
shonmisic <n.misic@outlook.com>
2021-11-12 10:13:15 +0100

    Initial commit

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        url: 'https://v77.tiktokcdn.com/cb58cbdb7ff273b54f04ccaaabc0d892/618e6c9e/video/tos/useast2a/tos-useast2a-pve-0068/89a108fdb04d4bcbbde53481fe457359/?a=1233&br=702&bt=351&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=wE~O8FLgkag3-I&l=202111120730230101880612190D021909&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M3dpcmY6Zng2ODMzNzczM0ApO2k0M2U1aTs1Nzo7OTk7NGcyYjEwcjQwcjJgLS1kMTZzcy8zL140NS9hLy4yNS8wYC86Yw%3D%3D&vl=&vr=',
+        channel: 'misicnenad',
+        description: 'MERN stack TikTok clone',
+        song: 'Some random TikTok song',
+        likes: '111',
+        shares: '222',
+        messages: '333',
+    },
+    {
+        url: 'https://v77.tiktokcdn.com/3bb7e0299f70162971154f57269a0591/618e6c90/video/tos/useast2a/tos-useast2a-ve-0068c002/f6a70a99f6894e198764979fd738406c/?a=1233&br=1456&bt=728&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=wE~O8FLgkag3-I&l=202111120730260101901911630801F884&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M2Y5O2Q6ZjpxNzMzNzczM0ApPDNlNTk4ZWU6N2lpOjpoaWdrZ2xxcjRvXm1gLS1kMTZzc18wNDFhXy4yMDYvXjAzLjY6Yw%3D%3D&vl=&vr=',
+        channel: 'misicnenad',
+        description: 'MERN stack TikTok clone 2',
+        song: 'Some random TikTok song 2',
+        likes: '234',
+        shares: '11',
+        messages: '432',
+    },
+]
+
+export default data
---
+++ server.js
@@ -0,0 +1,52 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import data from './data.js'
+import Videos from './dbModel.js'
+
+// app config
+const app = express()
+const port = 9000
+
+// middlewares
+app.use(express.json())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Headers', '*')
+
+    next()
+})
+
+// DB config
+const connection_url =
+    'mongodb+srv://admin:ZIqkoyAXfVrFTX25@cluster0.pplcf.mongodb.net/tiktok?retryWrites=true&w=majority'
+mongoose.connect(connection_url)
+
+// API endpoints
+app.get('/', (req, res) => res.status(200).send('hello world'))
+
+app.get('/v1/posts', (req, res) => res.status(200).send(data))
+
+app.get('/v2/posts', (req, res) => {
+    Videos.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    })
+})
+
+app.post('/v2/posts', (req, res) => {
+    const dbVideos = req.body
+
+    Videos.create(dbVideos, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    })
+})
+
+// listen
+app.listen(port, () => console.log(`listening on localhost:${port}`))
